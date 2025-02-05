0d6ca56429300ea0399acb68c39691e5425f481f
Dinakaran <rdina11598@gmail.com>
2025-02-05 10:04:43 +0530

    saved

---
+++ Javascript/lesson.js
@@ -0,0 +1,15 @@
+// var a = 10;
+// var b = 20;
+// if(a > b);
+// {
+//     console.log('true');
+// }else{
+//     console.log('false');
+// }
+
+var a = 1;
+while( a > 10);
+{
+    console.log('Hello World!');
+    a++ ;
+}
